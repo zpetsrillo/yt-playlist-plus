b995626fe74ee9c5b3bc43ff3be6c45f21a314fe
ZPetsrillo <zpetsrillo@gmail.com>
2020-04-27 14:25:06 -0500

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AngularFirestore } from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'song-rater';
+
+    constructor(private db: AngularFirestore) {
+      const things = db.collection('songs').valueChanges();
+      things.subscribe(console.log);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: "AIzaSyBLEwGz5lDQ1DKH2GFqD3fPqnXVHFp2l6o",
+  authDomain: "song-rater.firebaseapp.com",
+  databaseURL: "https://song-rater.firebaseio.com",
+  projectId: "song-rater",
+  storageBucket: "song-rater.appspot.com",
+  messagingSenderId: "923959586301",
+  appId: "1:923959586301:web:9bb56c6ae3a216d8f2fd94"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
